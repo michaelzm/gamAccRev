63cb646ace0ed3aae89c8ea731ec89d4dfd8dcbf
Michael Zemler <michael.zemler@hotmail.com>
2018-12-04 23:03:06 +0100

    updated dashboard
    updated dashboard to show real tracked data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AccessReviewComponent } from '../access-review/access-review.component'
+import { AccessReviewComponent } from '../access-review/access-review.component';
+import { UserService} from '../user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,27 @@ import { AccessReviewComponent } from '../access-review/access-review.component'
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  constructor() { }
+  dashboardScore: number;
+  dashboardCounter: number;
+  dashboardUserName: string;
+  constructor(private userService: UserService) { }
+
+  getScore(){
+    this.dashboardScore = this.userService.getUserScore();
+
+  }
+  getCounter(){
+    this.dashboardCounter = this.userService.getUserCounter();
+  }
+
+  getUserName(){
+    this.dashboardUserName = this.userService.getUserLName();
+  }
 
   ngOnInit() {
+    this.getCounter();
+    this.getScore();
+    this.getUserName();
   }
 
 }
