1d42118dedea7054471c4323307476ad0c5d9e7b
Michael Zemler <michael.zemler@hotmail.com>
2018-12-10 22:58:18 +0100

    material.ts imports

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,12 +1,15 @@
 import {MatButtonModule, MatCheckboxModule, MatMenuModule} from '@angular/material';
 import {MatTableModule} from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatMenuModule ,MatPaginatorModule, 
-    MatTableModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatMenuModule, MatPaginatorModule, MatTableModule],
+    MatTableModule, MatTabsModule, MatSnackBarModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatMenuModule, 
+    MatPaginatorModule, MatTableModule, MatTabsModule, MatSnackBarModule],
 })
 export class MaterialModule { }
