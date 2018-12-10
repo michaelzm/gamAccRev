1cc1c77d3388e5268698c35312db3a91f0186ce9
Michael Zemler <michael.zemler@hotmail.com>
2018-12-10 23:50:47 +0100

    Reworked login screen and menu layout

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,15 +1,18 @@
 import {MatButtonModule, MatCheckboxModule, MatMenuModule} from '@angular/material';
 import {MatTableModule} from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material';
+import {MatPaginatorModule } from '@angular/material';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatInputModule} from '@angular/material/input';
 
 import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatMenuModule ,MatPaginatorModule, 
-    MatTableModule, MatTabsModule, MatSnackBarModule],
+    MatTableModule, MatTabsModule, MatSnackBarModule,
+    MatInputModule],
   exports: [MatButtonModule, MatCheckboxModule, MatMenuModule, 
-    MatPaginatorModule, MatTableModule, MatTabsModule, MatSnackBarModule],
+    MatPaginatorModule, MatTableModule, MatTabsModule,
+    MatInputModule, MatSnackBarModule],
 })
 export class MaterialModule { }
