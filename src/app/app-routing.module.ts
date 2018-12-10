388c3c250c84e488e5c1e7e52245367f2b0f989e
Michael Zemler <michael.zemler@hotmail.com>
2018-12-10 20:20:45 +0100

    Added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccessReviewComponent } from './access-review/access-review.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
   { path: 'accessreview', component: AccessReviewComponent},
   { path: 'dashboard', component: DashboardComponent},
   { path: 'login', component: LoginComponent},
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'table', component: TableComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
