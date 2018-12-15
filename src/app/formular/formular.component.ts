deb9488197459e769de05ceb111b29674487fb90
Michael Zemler <michael.zemler@hotmail.com>
2018-12-15 14:48:10 +0100

    evaluation Formular component

---
+++ src/app/formular/formular.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+@Component({
+  selector: "app-formular",
+  templateUrl: "./formular.component.html",
+  styleUrls: ["./formular.component.css"]
+})
+export class FormularComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
