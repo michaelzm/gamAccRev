ede212d772adf0dda596e03c7b4a8e1aa89cc0b3
Michael Zemler <michael.zemler@hotmail.com>
2018-12-23 18:00:13 +0100

    Added fomular send feature

--- src/app/formular/formular.service.ts
+++ src/app/formular/formular.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Formular } from "./formular";
+import { Http, Response } from "@angular/http";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class FormularService {
+  private formularsUrl = "/api/formulars";
 
-  constructor() { }
+  constructor(private http: Http) {}
+  getFormulars(): Promise<void | Formular[]> {
+    return this.http
+      .get(this.formularsUrl)
+      .toPromise()
+      .then(response => response.json() as Formular[])
+      .catch(this.handleError);
+  }
+  createFormular(newFormular: Formular): Promise<void | Formular> {
+    return this.http
+      .post(this.formularsUrl, newFormular)
+      .toPromise()
+      .then(response => response.json() as Formular)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any) {
+    let errMsg = error.message
+      ? error.message
+      : error.status
+      ? `${error.status} - ${error.statusText}`
+      : "Server error";
+    console.error(errMsg); // log to console instead
+  }
 }
