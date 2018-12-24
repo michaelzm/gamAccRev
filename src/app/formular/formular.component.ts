b2a73b4d7b79de926526c9d8193579744323d0a0
Michael Zemler <michael.zemler@hotmail.com>
2018-12-25 00:59:09 +0100

    Added formular class and functionality to component

--- src/app/formular/formular.component.ts
+++ src/app/formular/formular.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { ConfigService } from "../config/config.service";
+import { Formular } from "./formular";
 @Component({
   selector: "app-formular",
   templateUrl: "./formular.component.html",
   styleUrls: ["./formular.component.css"]
 })
 export class FormularComponent implements OnInit {
-  constructor() {}
+  submitFormular: Formular;
+
+  constructor(private config: ConfigService) {}
 
   ngOnInit() {}
+
+  onSubmitEvaluation() {
+    console.log("starte submit");
+
+    this.submitFormular = new Formular();
+    this.submitFormular.name = "Landodger";
+    console.log(this.submitFormular);
+    this.config.postConfig(this.submitFormular).subscribe();
+  }
 }
---
+++ src/app/formular/formular.ts
@@ -0,0 +1,3 @@
+export class Formular {
+  name: String;
+}
