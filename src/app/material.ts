b5c76dd122dd57fcc82ee678d4193242b5130388
Michael Zemler <michael.zemler@hotmail.com>
2018-12-25 18:05:47 +0100

    imports for formular

--- src/app/material.ts
+++ src/app/material.ts
@@ -14,6 +14,7 @@ import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatStepperModule } from "@angular/material/stepper";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSliderModule } from "@angular/material/slider";
 
 import { NgModule } from "@angular/core";
 
@@ -32,7 +33,8 @@ import { NgModule } from "@angular/core";
     MatProgressBarModule,
     MatFormFieldModule,
     MatStepperModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSliderModule
   ],
   exports: [
     MatButtonModule,
@@ -48,7 +50,8 @@ import { NgModule } from "@angular/core";
     MatProgressBarModule,
     MatFormFieldModule,
     MatStepperModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSliderModule
   ]
 })
 export class MaterialModule {}
