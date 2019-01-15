cf8048fb2156b79ad44d066359cfcf4f90fd4334
Michael Zemler <michael.zemler@hotmail.com>
2019-01-15 23:52:27 +0100

    new formular data

--- src/app/formular/formular.component.ts
+++ src/app/formular/formular.component.ts
@@ -25,34 +25,43 @@ export class FormularComponent implements OnInit {
   submitFormular: Formular;
   rankingFormular: Competitor;
 
-  rating1: number;
-  rating2: number;
-  rating3: number;
-  rating4: number;
+  parta1: number;
+  parta2: number;
+  partb1: number;
+  partb2: number;
+  partb3: number;
+  partb4: number;
+  partb5: number;
+  partb6: number;
   /*
   atm not working as expected
   */
 
-  onInputChangeFirst(value: any, slider: string) {
-    console.log(value);
-    this.rating1 = value;
-  }
   constructor(
     private config: ConfigService,
     private userSerice: UserService,
     private missionService: MissionService
   ) {}
-
+  ngOnInit() {}
+  //think about switch case maybe
   onInputChange(event: any, slider: number) {
     console.log(event);
     if (slider == 1) {
-      this.rating1 = event.value;
+      this.parta1 = event.value;
     } else if (slider == 2) {
-      this.rating2 = event.value;
+      this.parta2 = event.value;
     } else if (slider == 3) {
-      this.rating3 = event.value;
+      this.partb1 = event.value;
     } else if (slider == 4) {
-      this.rating4 = event.value;
+      this.partb2 = event.value;
+    } else if (slider == 5) {
+      this.partb3 = event.value;
+    } else if (slider == 6) {
+      this.partb4 = event.value;
+    } else if (slider == 7) {
+      this.partb5 = event.value;
+    } else if (slider == 8) {
+      this.partb6 = event.value;
     } else {
       console.log("error occured, slider not registered");
     }
@@ -65,19 +74,24 @@ export class FormularComponent implements OnInit {
     this.config.postRanking(this.rankingFormular).subscribe();
   }
 
-  ngOnInit() {}
   onSubmitEvaluation() {
     if (this.hasbeenSubmitted == false) {
       console.log("starte submit");
       this.submitFormular = new Formular();
       this.submitFormular.name = this.userSerice.getUserLName();
       this.submitFormular.reviewCount = this.userSerice.getUserCounter();
-      this.submitFormular.rating1 = this.rating1 || 0;
-      this.submitFormular.rating2 = this.rating2 || 0;
-      this.submitFormular.rating3 = this.rating3 || 0;
-      this.submitFormular.rating4 = this.rating4 || 0;
+      this.submitFormular.userLevel = this.userSerice.getUserLevel();
+      this.submitFormular.userRanking = this.userSerice.getRanking();
+      this.submitFormular.partA1 = this.parta1 || 0;
+      this.submitFormular.partA2 = this.parta2 || 0;
+      this.submitFormular.partB1 = this.partb1 || 0;
+      this.submitFormular.partB2 = this.partb2 || 0;
+      this.submitFormular.partB3 = this.partb3 || 0;
+      this.submitFormular.partB4 = this.partb4 || 0;
+      this.submitFormular.partB5 = this.partb5 || 0;
+      this.submitFormular.partB6 = this.partb6 || 0;
       console.log(this.submitFormular);
-      this.config.postConfig(this.submitFormular).subscribe();
+      this.config.postGamification(this.submitFormular).subscribe();
       this.submitRanking();
       this.hasbeenSubmitted = true;
       this.missionService.setFormularDone();
--- src/app/formular/formular.ts
+++ src/app/formular/formular.ts
@@ -1,8 +1,14 @@
 export class Formular {
   name: String;
   reviewCount: number;
-  rating1: number;
-  rating2: number;
-  rating3: number;
-  rating4: number;
+  userLevel: number;
+  userRanking: number;
+  partA1: number;
+  partA2: number;
+  partB1: number;
+  partB2: number;
+  partB3: number;
+  partB4: number;
+  partB5: number;
+  partB6: number;
 }
