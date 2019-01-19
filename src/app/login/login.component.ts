2f642a0897c94487e37942bd4afacb0d59ad1810
Michael Zemler <michael.zemler@hotmail.com>
2019-01-19 23:06:27 +0100

    No more name required, sets default name

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   }
 
   login(name: HTMLInputElement) {
-    this.userService.setUserLastName(name.value);
+    this.userService.setUserLastName("basicSoNoName");
     return false;
   }
   checkIfShowFormField() {
